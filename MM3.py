7dd540509c339915e1711c0d0f9243ef3ebd42b7
vdr <jedziukas14@gmail.com>
2021-09-26 13:22:20 +0200

    MM3, MM4 exercises

---
+++ MM3.py
@@ -0,0 +1,148 @@
+'''
+The aim of this exercise is to classify points to either one multivariate distribution or the other.
+The program evaluates the probability of one distribution and the other distribution and then decides
+to which class do the points belong to.
+
+Method used: MLE and MAP. Considering if we know something prior about our data or not.
+'''
+
+import pandas as pd
+import numpy as np
+from scipy.stats import multivariate_normal
+import matplotlib.pyplot as plt
+
+def load_np(df):
+    numpy = np.asarray(df.values)
+    return numpy
+def plot_points(numpy):
+    x, y = numpy.T
+    return x, y
+def params(numpy):
+    mean = np.mean(numpy, axis=0)
+    std = np.std(numpy)
+    var = np.var(numpy)
+    cov = np.cov(numpy.T)
+    return mean, std, var, cov
+def compare_classes(x_pdf, y_pdf):
+    class_1 = []
+    x_pdf_list = x_pdf.tolist()
+    y_pdf_list = y_pdf.tolist()
+
+    for i in range(len(x_pdf_list)):
+        if x_pdf_list[i] > y_pdf_list[i]:
+            class_1.append('1')
+        else:
+            class_1.append('2')
+    return class_1
+def compare_for_plotting(class_1, xy_test):
+    '''
+    :param class_1: likelihood class label
+    :param xy_test: real data
+    :return: a list of points belonging to X distribution and Y distribution
+    '''
+    plot_list_1 = []
+    plot_list_2 = []
+    xy_data = xy_test.tolist()
+    for i in range(len(class_1)):
+        if class_1[i] == '1':
+            plot_list_1.append(xy_data[i])
+        else:
+            plot_list_2.append(xy_data[i])
+    plt_numpy_1 = np.asarray(plot_list_1)
+    plt_numpy_2 = np.asarray(plot_list_2)
+    return plt_numpy_1, plt_numpy_2
+def accuracy(class_comparison, test_class):
+    class_np = np.asarray(class_comparison).T
+    gg = np.subtract(class_np.astype(int), test_class.T)
+    true_negative = np.sum(abs(gg))
+    true_positive = class_np.size - true_negative
+    accuracy = true_positive / class_np.size
+    return accuracy
+def rate(x_train, y_train):
+    sum = len(x_train) + len(y_train)
+    rate_x = len(x_train) / sum
+    rate_y = 1 - rate_x
+    return rate_x, rate_y
+
+######Loading the datasets######
+df = pd.read_csv('MM3_material/trn_x.txt', sep='  ', names=['col1', 'col2'])
+df_1 = pd.read_csv('MM3_material/trn_y.txt', sep='  ', names=['col1', 'col2'])
+df_3 = pd.read_csv('MM3_material/tst_xy.txt', sep='  ', names=['col1', 'col2'])
+df_4 = pd.read_csv('MM3_material/tst_xy_class.txt', sep='  ', names=['col1'])
+df_5 = pd.read_csv('MM3_material/tst_xy_126.txt', sep='  ', names=['col1', 'col2'])
+df_6 = pd.read_csv('MM3_material/tst_xy_126_class.txt', sep='  ', names=['col1'])
+
+##### Loading DF's as np arrays ####
+x_train = load_np(df)
+y_train = load_np(df_1)
+xy_test = load_np(df_3)
+xy_test_class = load_np(df_4)
+xy_126_test = load_np(df_5)
+xy_126_class = load_np(df_6)
+
+##### Estimating parameters for distributions #####
+mean_x, std_x, var_x, cov_x = params(x_train)
+mean_y, std_y, var_y, cov_y = params(y_train)
+
+##### Creating the distributions #####
+l_x = multivariate_normal(mean=mean_x, cov = cov_x) #multivariate distribution for X (consists of X and Y points)
+l_y = multivariate_normal(mean=mean_y, cov = cov_y) #second multivariate distribution for Y (consists of X and Y points)
+
+x_rate, y_rate = rate(x_train, y_train) #calculating the dataset rate
+
+''' 
+Posterior density formula when prior is given:
+P(theta|X) = (P(X|theta) * P(theta)) / P(X)
+theta_MAP = argmax_theta(P(theta|x))
+
+Posteriori density formula when no prior is given:
+P(theta|X) = P(X|theta)
+where theta is defined as theta_ML = argmax_theta(P(X|theta))
+as no prior is given.
+Thus parameters are set up by solving the ML equation for multivariate distribution for N random variables.
+
+multivariate_normal.pdf() function solves for ML, thus if any prior is given - then we can multiply the probability density
+function with given prior probabilities
+'''
+
+#Exercise 1
+#Maximum a posteriori (MAP) as we know the length of both data sets and we can find the rate
+x_pdf = l_x.pdf(xy_test) * x_rate
+y_pdf = l_y.pdf(xy_test) * y_rate
+
+class_1 = compare_classes(x_pdf, y_pdf)
+model_accuracy = accuracy(class_1, xy_test_class)
+
+#Exercise 2
+#Maximum Likelihood (ML), as no prior probabilities are given.
+#We can ignore the posterior, or uniformly distribute by 1/2 and 1/2. The result does not change.
+x_pdf_2 = l_x.pdf(xy_126_test)
+y_pdf_2 = l_y.pdf(xy_126_test)
+
+class_2 = compare_classes(x_pdf_2, y_pdf_2)
+model_accuracy_2 = accuracy(class_2, xy_126_class)
+
+#Exercise 3
+#Maximum a posteriori (MAP) assuming prior probability for X = 0.9 and y = 0.1
+x_pdf_3 = l_x.pdf(xy_126_test) * 0.9
+y_pdf_3 = l_y.pdf(xy_126_test) * 0.1
+
+class_3 = compare_classes(x_pdf_3, y_pdf_3)
+model_accuracy_3 = accuracy(class_3, xy_126_class)
+print('Accuracy difference: {0} %'.format((model_accuracy_3-model_accuracy_2)*100))
+
+######### FOR PLOTTING ############
+#change the class argument for plotting comparison (class_1, class_2, class_3)
+x_classified, y_classified = compare_for_plotting(class_1,xy_test)
+x_1, x_2 = plot_points(x_train)
+y_1, y_2 = plot_points(y_train)
+x_c1, x_c2 = plot_points(x_classified)
+y_c1, y_c2 = plot_points(y_classified)
+
+plt.scatter(x_1, x_2, edgecolors='blue')
+plt.scatter(y_1, y_2, edgecolors='green')
+plt.scatter(x_c1, x_c2, edgecolors='red')
+#plt.scatter(y_c1, y_c2, edgecolors='yellow')  # Has outliers
+plt.show()
+
+
---
+++ MM4.py
@@ -0,0 +1,95 @@
+'''
+PCA script to reduce the dimensionality of the data set to 2 dimensions and then transform class data labels to PCA subspace.
+Steps:
+1. Concatinate training class labels into one array (class 5, 6 and 8)
+2. Calculate the mean for every dimension and compute a covariance matrix
+3. Take 2 eigenvectors with largest eigenvalues (take as many eigenvectors as dimensions that you want to reduce to)
+4. Calculate PCA subspace using this formula z = w.T@(x - mean).T , where w.T is a matrix of eigenvectors. Save eigenvectors and mean of concatinated set
+5. Fit test class labels (5, 6  and 8) to PCA's eigenvectors and mean.
+6. Plot it and see how well did we classify it.
+'''
+import numpy as np
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+
+def to_3_classes(data, train=True):
+    '''
+    :param data: raw data set
+    :param train: Boolean for a train set
+    :return: either train classes or test classes
+    '''
+    if train:
+        class_5 = data['train5']
+        class_6 = data['train6']
+        class_8 = data['train8']
+        return class_5, class_6, class_8
+    class_5 = data['test5']
+    class_6 = data['test6']
+    class_8 = data['test8']
+    return class_5, class_6, class_8
+def params(data):
+    '''
+    :param data: pass a numpy array
+    :return: returns covariance matrix and mean of the data
+    '''
+    cov = np.cov(data.T)  #784 pivot entries
+    mean = np.mean(data, axis=0) #784 dimensions
+    return cov, mean
+def dimensionality_reduction(x, mean, cov, dimensions):
+    '''
+    :param x: raw data (numpy array)
+    :param mean: mean of raw data (numpy array)
+    :param cov: covariance of raw data (numpy array mxm)
+    :param dimensions: number of dimensions to reduce
+    :return: Principal component space and largest eigenvectors
+    '''
+    eigen_val, eigen_vec = np.linalg.eig(cov)
+    w = eigen_vec[0:,:dimensions]
+    z = w.T@(x - mean).T
+    return z.T, w
+
+#Loading the data and extracting individual classes
+data = loadmat('MM4_material/mnist_all.mat')
+c_5, c_6, c_8 = to_3_classes(data, train=True)
+c_5_t, c_6_t, c_8_t = to_3_classes(data, train=False)
+
+#Concatinating train classes into one and then calculating the covariance matrix and mean of the set
+train_concat = np.concatenate([c_5, c_6, c_8])
+train_concat_cov, train_concat_mean = params(train_concat)
+
+#Establishing a PCA subspace
+c_2_dim, train_eigenvector = dimensionality_reduction(x= train_concat, mean= train_concat_mean, cov= train_concat_cov, dimensions=3)
+
+#Transforming class 5 points to PCA subspace
+class_5_test = train_eigenvector.T@(c_5_t - train_concat_mean).T
+class_5_test = class_5_test.T
+
+#Transforming class 6 points to PCA subspace
+class_6_test = train_eigenvector.T@(c_6_t - train_concat_mean).T
+class_6_test = class_6_test.T
+
+#Transforming class 8 points to PCA subspace
+class_8_test = train_eigenvector.T@(c_8_t - train_concat_mean).T
+class_8_test = class_8_test.T
+
+#### PLOTTING ####
+plt.scatter(x=c_2_dim[:,0], y=c_2_dim[:,1])  #PCA subspace
+plt.scatter(x=class_5_test[:,0], y=class_5_test[:,1], color='green')
+plt.scatter(x=class_6_test[:,0], y=class_6_test[:,1], color='red')
+plt.scatter(x=class_8_test[:,0], y=class_8_test[:,1], color='yellow')
+plt.show()
+
+
+#### PLOTTING 3D ####
+# For plotting in 3D change the dimensionality of PCA subspace to 3.
+
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+#
+# ax.scatter(c_2_dim[:,0], c_2_dim[:,1], c_2_dim[:,2], cmap='blue')
+# ax.scatter(class_5_test[:,0], class_5_test[:,1], class_5_test[:,2], cmap='green')
+# ax.scatter(class_6_test[:,0], class_6_test[:,1], class_6_test[:,2], cmap='red')
+# ax.scatter(class_8_test[:,0], class_8_test[:,1], class_8_test[:,2], cmap='yellow')
+# plt.show()
+
+
