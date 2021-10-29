5e422b4faf15c3dddf1513778bafb83f45924efb
vdr <jedziukas14@gmail.com>
2021-10-29 15:12:27 +0200

    Added comments and todo list

--- MM8_manual.py
+++ MM8_manual.py
@@ -1,11 +1,16 @@
+'''
+Deep learning of MNIST dataset.
+Currently implemented activation functions: Relu and Sigmoid
+Total number of layers: 3
+'''
 from scipy.io import loadmat
 import numpy as np
 import logging
 import datetime
 from sklearn.base import BaseEstimator
-from sklearn.utils import shuffle
 from sklearn.preprocessing import OneHotEncoder
 from scipy.stats import truncnorm
+from _overused_functions import Overused
 
 # Logger
 logger = logging.getLogger()
@@ -18,38 +23,19 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
 
+#TODO: Add evaluation metrics through epochs
+#TODO: Make it viable with more layers than 1. Currently if more than 1 added - accuracy gets poorer. Find out why.
+
+
 def truncated_normal(mean=0, sd=1, low=0, upp=10):
     return truncnorm((low-mean)/sd, (upp-mean)/sd, loc=mean, scale=sd)
 
-def to_sep(data, to_shuffle=True):
-    x_train = []
-    x_test = []
-    for i in range(10):
-        x_train.append(data['train%d' % i])
-        x_test.append(data['test%d' % i])
-
-    accuracy_target_classes = [i*np.ones(len(x_test[i])) for i in range(10)]
-
-    train_concat = np.concatenate(x_train)
-    test_concat = np.concatenate(x_test)
-
-    # Train labels
-    train_labels = [np.full(len(x_train[i]), i) for i in range(10)]
-    train_labels = np.concatenate(train_labels)
-
-    # Test labels
-    test_labels = [np.full(len(x_test[i]), i) for i in range(10)]
-    test_labels = np.concatenate(test_labels)
-
-    if to_shuffle:
-        train_shuffle, train_shuffle_labels = shuffle(train_concat, train_labels)
-        return train_shuffle, train_shuffle_labels, test_concat, test_labels, accuracy_target_classes
-    return train_concat, test_concat, x_test, test_labels, accuracy_target_classes
-
 class MultilayerPerceptron(BaseEstimator):
-    def __init__(self, inputLayer, hiddenLayer, outputLayer, learningRate, activation_func, max_epochs, classes_number, bias=True):
+    def __init__(self, inputLayer, hiddenLayer_1, outputLayer, learningRate, activation_func, max_epochs, classes_number, bias=True):
         self.inputLayer = inputLayer
-        self.hiddenLayer = hiddenLayer
+        self.hiddenLayer_1 = hiddenLayer_1
+        # self.hiddenLayer_2 = hiddenLayer_2
+        # self.hiddenLayer_3 = hiddenLayer_3
         self.outputLayer = outputLayer
         self.learningRate = learningRate
         self.max_epochs = max_epochs
@@ -74,11 +60,19 @@ class MultilayerPerceptron(BaseEstimator):
     def create_weight_matrices(self):
         rad = 1 / np.sqrt(self.inputLayer + self.bias)
         X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
-        self.weight_hidden = X.rvs((self.hiddenLayer, self.inputLayer + self.bias))
+        self.weight_hidden_1 = X.rvs((self.hiddenLayer_1, self.inputLayer + self.bias))
 
-        rad = 1 / np.sqrt(self.hiddenLayer + self.bias)
+        # rad = 1 / np.sqrt(self.hiddenLayer_1 + self.bias)
+        # X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
+        # self.weight_hidden_2 = X.rvs((self.hiddenLayer_2, self.hiddenLayer_1 + self.bias))
+        #
+        # rad = 1 / np.sqrt(self.hiddenLayer_2 + self.bias)
+        # X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
+        # self.weight_hidden_3 = X.rvs((self.hiddenLayer_3, self.hiddenLayer_2 + self.bias))
+
+        rad = 1 / np.sqrt(self.hiddenLayer_1 + self.bias)
         X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
-        self.weight_output = X.rvs((self.outputLayer, self.hiddenLayer + self.bias))
+        self.weight_output = X.rvs((self.outputLayer, self.hiddenLayer_1 + self.bias))
     def train_single_vector(self, input_vector, target_vector):
         # Adding bias to the end of input vector
         if self.bias != 0:
@@ -86,15 +80,24 @@ class MultilayerPerceptron(BaseEstimator):
         input_vector_t = np.array(input_vector, ndmin=2).T
         target_vector_t = np.array(target_vector, ndmin=2).T
 
-        self.output_1 = self.activation((np.dot(self.weight_hidden, input_vector_t)))
+        self.output_hidden_1 = self.activation((np.dot(self.weight_hidden_1, input_vector_t)))
 
-        # adding bias to the end of hidden layer
+        # # adding bias to the end of hidden layer
         if self.bias != 0:
-            self.output_1 = np.concatenate((self.output_1, [[self.bias]]))
+            self.output_hidden_1 = np.concatenate((self.output_hidden_1, [[self.bias]]))
+        #
+        # self.output_hidden_2 = self.activation((np.dot(self.weight_hidden_2, self.output_hidden_1)))
+        # if self.bias != 0:
+        #     self.output_hidden_2 = np.concatenate((self.output_hidden_2, [[self.bias]]))
+        # self.output_hidden_3 = self.activation((np.dot(self.weight_hidden_3, self.output_hidden_2)))
+        # if self.bias != 0:
+        #     self.output_hidden_3 = np.concatenate((self.output_hidden_3, [[self.bias]]))
 
-        self.output_2 = self.activation((np.dot(self.weight_output, self.output_1)))
 
-        error = (target_vector - self.output_2)
+
+        self.output_layer = self.activation((np.dot(self.weight_output, self.output_hidden_1)))
+
+        error = (target_vector - self.output_layer)
         total_error = sum(error)/len(input_vector)
 
         # Backpropagation
@@ -120,18 +123,37 @@ class MultilayerPerceptron(BaseEstimator):
         # calculating the error = (weight_k * error_j)
         # where k is hidden_layer and j is output_layer
 
-        error_output = target_vector - self.output_2
-        tmp = error_output * self.deriv(self.output_2)
-        self.weight_output += self.learningRate * np.dot(tmp, self.output_1.T)
-        # Updating weight Output -> Hidden
+        error_output = target_vector - self.output_layer
+        tmp = error_output * self.deriv(self.output_layer)
+        self.weight_output += self.learningRate * np.dot(tmp, self.output_hidden_1.T) # output_hidden_3
+
+
+
+
+
+        # hidden_error = np.dot(self.weight_output.T, error_output)
+        # tmp = hidden_error * self.deriv(self.output_hidden_3)
+        # if self.bias != 0:
+        #     self.weight_hidden_3 += self.learningRate * np.dot(tmp, self.output_hidden_2.T)[:-1, :]
+        #
+        # hidden_error_2 = np.dot(self.weight_hidden_3.T, hidden_error[:-1, :])
+        # #error_output_2 = hidden_error_2 - self.output_hidden_2
+        # tmp = hidden_error_2 * self.deriv(self.output_hidden_2)
+        # if self.bias != 0:
+        #     self.weight_hidden_2 += self.learningRate * np.dot(tmp, self.output_hidden_1.T)[:-1,:]
+
+
+        # hidden_error_3 = np.dot(self.weight_hidden_2.T, hidden_error_2[:-1, :])
+        # tmp = hidden_error_3 * self.deriv(self.output_hidden_1)
 
         hidden_error = np.dot(self.weight_output.T, error_output)
-        tmp = hidden_error * self.deriv(self.output_1)
+        tmp = hidden_error * self.deriv(self.output_hidden_1)
         if self.bias != 0:
             # removing the last row to get rid of 0's
-            self.weight_hidden += self.learningRate * np.dot(tmp, inputs.T)[:-1,:]
+            self.weight_hidden_1 += self.learningRate * np.dot(tmp, inputs.T)[:-1, :]
         else:
-            self.weight_hidden += self.learningRate * np.dot(tmp, inputs.T)
+            self.weight_hidden_1 += self.learningRate * np.dot(tmp, inputs.T)
+
     def predict(self, input_vector):
         predictions = []
         for i in range(len(input_vector)):
@@ -139,19 +161,29 @@ class MultilayerPerceptron(BaseEstimator):
             if self.bias != 0:
                 input_vector_input = np.concatenate((input_vector[i], [self.bias]))
             input_vector_t = np.array(input_vector_input, ndmin=2).T
-            self.output_1 = self.activation((np.dot(self.weight_hidden, input_vector_t)))
+            self.output_hidden_1 = self.activation((np.dot(self.weight_hidden_1, input_vector_t)))
             if self.bias != 0:
-                self.output_1 = np.concatenate((self.output_1, [[self.bias]]))
+                self.output_hidden_1 = np.concatenate((self.output_hidden_1, [[self.bias]]))
 
             # Argmax to decide which neuron is the biggest
-            self.output_2 = self.activation((np.dot(self.weight_output, self.output_1)))
-            predictions.append(np.argmax(self.output_2))
+            # self.output_layer = self.activation((np.dot(self.weight_output, self.output_hidden_1)))
+            # self.output_hidden_2 = self.activation((np.dot(self.weight_hidden_2, self.output_hidden_1)))
+            # if self.bias != 0:
+            #     self.output_hidden_2 = np.concatenate((self.output_hidden_2, [[self.bias]]))
+            #
+            # self.output_hidden_3 = self.activation((np.dot(self.weight_hidden_3, self.output_hidden_2)))
+            # if self.bias != 0:
+            #     self.output_hidden_3 = np.concatenate((self.output_hidden_3, [[self.bias]]))
+
+            self.output_layer = self.activation((np.dot(self.weight_output, self.output_hidden_1)))
+
+            predictions.append(np.argmax(self.output_layer))
         return np.asarray(predictions)
 
 
 data = loadmat('MM4_material/mnist_all.mat')
 
-train_data, train_labels, test_data, test_labels, accuracy_target_classes = to_sep(data, to_shuffle=True)
+train_data, train_labels, test_data, test_labels, accuracy_target_classes = Overused().to_sep(data, to_shuffle=True)
 
 fac = 0.99/255
 train_imgs = np.asfarray(train_data) * fac + 0.01
@@ -164,7 +196,7 @@ test_onehot = onehot_encoder.fit_transform(test_labels.reshape(-1,1))
 train_onehot[train_onehot==0] = 0.01
 train_onehot[train_onehot==1] = 0.99
 
-MLP = MultilayerPerceptron(inputLayer=784, hiddenLayer=100, outputLayer=10, learningRate=0.1, max_epochs=5, activation_func='Sigmoid', classes_number=10, bias=True)
+MLP = MultilayerPerceptron(inputLayer=784, hiddenLayer_1=20, outputLayer=10, learningRate=0.1, max_epochs=5, activation_func='Sigmoid', classes_number=10, bias=True)
 MLP_fit = MLP.fit(train_imgs, train_onehot)
 prediction_labels = MLP_fit.predict(test_imgs)
 
